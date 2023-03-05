b88d40959888ae01dd695801fc435fd0321416e1
Ranjit Thota <amailtoranjith@gmail.com>
2023-03-05 12:24:43 +0100

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, NavLink } from "react-router-dom";
+import "./App.css";
+import Cart from "./Pages/Cart";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+
+  console.log(isOpen);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h2 className="logo">OrgaNickel</h2>
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+
+        <div className={isOpen ? "nav-links open" : "nav-links"}>
+          <NavLink to="/" className="nav-bar">
+            Home
+          </NavLink>
+          <NavLink to="/products" className="nav-bar">
+            Products
+          </NavLink>
+          <NavLink to="/cart" className="nav-bar">
+            <AiOutlineShoppingCart size="1.4em" />3
+          </NavLink>
+        </div>
+      </div>
+      <Routes className="routes">
+        <Route className="route" path="/" element={<Home></Home>}></Route>
+        <Route
+          className="route"
+          path="/products"
+          element={<Products></Products>}
+        ></Route>
+        <Route className="route" path="/cart" element={<Cart></Cart>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Product.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div>
+      <h2>Product</h2>
+    </div>
+  );
+}
