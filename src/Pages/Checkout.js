be1b5c557a9428c776014a61eaffe2e10d907139
Ranjit Thota <amailtoranjith@gmail.com>
2023-03-08 23:27:27 +0100

    added dummy payment gateway

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ProductContext } from "../Context/Context";
 import "../App.css";
 import "./Checkout.css";
@@ -7,9 +7,10 @@ import { useNavigate } from "react-router-dom";
 export default function Checkout() {
   const { kartPrice, user } = useContext(ProductContext);
   const navigate = useNavigate();
+  const [readyToPay, setReadyToPay] = useState(false);
 
   return (
-    <div>
+    <div className="checkout-parent">
       {kartPrice !== 0 && user && (
         <div>
           {" "}
@@ -28,6 +29,7 @@ export default function Checkout() {
               className="delivery-address"
               onSubmit={(e) => {
                 e.preventDefault();
+                setReadyToPay(true);
               }}
             >
               <input type="text" placeholder="Full Name" />
@@ -36,6 +38,9 @@ export default function Checkout() {
               <input type="text" placeholder="Street" />
               <input type="text" placeholder="City" />
               <input type="number" placeholder="Pin Code" />
+              <label>
+                <input type="checkbox" required /> I confirm my delivery address
+              </label>
               <button>Continue to Pay</button>
             </form>
           </div>
@@ -44,8 +49,76 @@ export default function Checkout() {
       {kartPrice <= 0 && navigate(`/products`)}
       {user.length === 0 && (
         <div>
-          <h2>Checkout</h2>
-          <p>Login Please</p>
+          <h2>Check Out</h2>
+          <p>Please Login to Check Out</p>
+        </div>
+      )}
+      {readyToPay && (
+        <div className="payment-modal">
+          <form className="paynow-form">
+            <h3>Debit/Credit Card</h3>
+
+            <input type="text" placeholder="Name on the card" required />
+            <input
+              placeholder="Card number"
+              type="number"
+              pattern="[0-9]{12,}"
+              inputMode="numeric"
+              minLength="12"
+              maxLength="12"
+              required
+            />
+
+            <div className="expiry-CVV">
+              <select className="month" name="month" defaultValue="January">
+                <option>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+              </select>
+              <select className="month" name="month" defaultValue="2023">
+                <option>2023</option>
+                <option>2024</option>
+                <option>2025</option>
+                <option>2026</option>
+                <option>2027</option>
+                <option>2028</option>
+                <option>2029</option>
+              </select>
+              <input
+                type="password"
+                placeholder="CVV"
+                className="CVV"
+                style={{ width: "50px" }}
+                pattern="\d{3}"
+                required
+              />
+            </div>
+
+            <button>Pay Now</button>
+            <button
+              style={{
+                cursor: "pointer",
+                marginTop: "15px",
+                backgroundColor: "white",
+                border: "none",
+                color: "red",
+              }}
+              onClick={() => {
+                setReadyToPay(false);
+              }}
+            >
+              Cancel
+            </button>
+          </form>
         </div>
       )}
     </div>
